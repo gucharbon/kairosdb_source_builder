870377e796a9819172ca6194c19e5dc31521e9d2
gucharbon <36370689+gucharbon@users.noreply.github.com>
2018-02-11 15:10:02 +0100

    Changed comments

--- build_kairos_sources.sh
+++ build_kairos_sources.sh
@@ -4,20 +4,19 @@ set -e
 set -u
 
 BASE_DIR=$(readlink -m $(dirname $0))
-echo "Moving to project root directory : ${BASE_DIR}"
 cd $BASE_DIR
 
 
 kairos_sources_dir=$BASE_DIR/kairos_sources
-echo "KairosDB sources will be available in the dist_volume located at : $kairos_sources_dir"
+echo "KairosDB sources will be available in the kairos_source directory located at : $kairos_sources_dir"
 
 mkdir -p $kairos_sources_dir
 
+echo "Building docker image"
 docker build . -t kairos_installation
 
+echo "Running docker container and copying KairosDB sources"
 docker run -d --name build_kairos --rm  kairos_installation && \
 docker cp build_kairos:/root/kairosdb/build/.  $kairos_sources_dir/. 
-
-
 
 exit 0 
